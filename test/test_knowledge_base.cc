0aa123a887d9c4a0763cbb6089b7d9d604bcb667
Ryuya Ikeda <rikeda71@gmail.com>
2019-11-12 15:42:04 +0900

    Add: knowledgebase test

--- test/test_knowledge_base.cc
+++ test/test_knowledge_base.cc
@@ -27,10 +27,34 @@ class TestKnowledgeBase : public ::testing::Test {
   knowledge_base::KnowledgeBaseRE *klbr;
 };
 
-TEST_F(TestKnowledgeBase, Initializing) {
-  std::set<std::pair<std::string, std::string>> st;
+TEST_F(TestKnowledgeBase, InitializingTest) {
   EXPECT_GT(klbn->knowledges.size(), 0);
   EXPECT_GT(klbn->entities.size(), 0);
   EXPECT_GT(klbr->knowledges.size(), 0);
   EXPECT_GT(klbr->entities.size(), 0);
+}
+
+TEST_F(TestKnowledgeBase, LoadingTest) {
+  // test of existing entities
+  EXPECT_TRUE(klbn->entities.find("Obama") != klbn->entities.end());
+  EXPECT_TRUE(klbn->entities.find("Trump") != klbn->entities.end());
+  EXPECT_TRUE(klbn->entities.find("Bush") == klbn->entities.end());
+  EXPECT_TRUE(klbr->entities.find("Obama") != klbr->entities.end());
+  EXPECT_TRUE(klbr->entities.find("Hawaii") != klbr->entities.end());
+  EXPECT_TRUE(klbr->entities.find("Bush") == klbr->entities.end());
+
+  // test of existing knowledges
+  knowledge_base::nepair np1 = std::make_pair("Obama", "PER");
+  knowledge_base::nepair np2 = std::make_pair("Hawaii", "LOC");
+  knowledge_base::nepair np3 = std::make_pair("Bush", "PER");
+  EXPECT_TRUE(klbn->knowledges.find(np1) != klbn->knowledges.end());
+  EXPECT_TRUE(klbn->knowledges.find(np2) != klbn->knowledges.end());
+  EXPECT_TRUE(klbn->knowledges.find(np3) == klbn->knowledges.end());
+  knowledge_base::retuple rt1 = std::make_tuple("Obama", "Hawaii", "LiveIn");
+  knowledge_base::retuple rt2 = std::make_tuple("Trump", "NewYork", "BornIn");
+  knowledge_base::retuple rt3 =
+      std::make_tuple("Bush", "Connecticut", "BornIn");
+  EXPECT_TRUE(klbr->knowledges.find(rt1) != klbr->knowledges.end());
+  EXPECT_TRUE(klbr->knowledges.find(rt2) != klbr->knowledges.end());
+  EXPECT_TRUE(klbr->knowledges.find(rt3) == klbr->knowledges.end());
 }
